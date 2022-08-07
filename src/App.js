d764a7a1bf7a15af3c747462b370bb0b6c6ab5d4
ConradQQ <connorhawkesps4@gmail.com>
2022-08-07 13:51:34 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
+const getQuotes = () => {
+fetch('https://type.fit/api/quotes')
+	.then(response => response.json())
+	.then((data) => {
+    let randomIndex = Math.floor((Math.random() * data.length));
+
+    document.getElementById('text').innerHTML = `"${data[randomIndex].text}"`
+    document.getElementById('author').innerHTML = `- ${data[randomIndex].author}`
+    
+  })
+	.catch(err => console.error(err));
+ 
+}
+
+
+
+
+getQuotes()
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return (    
+    <div id="quote-box" className="d-grid gap-3">
+      <div className="row">
+        <div className="col-sm-12">
+        <h1 id="title"></h1>
+        </div>
+        
+      </div>
+      <div className="row">
+        <div className="col-sm-12 mx-xl-2">
+          <h4 id="text"></h4>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12">
+        <p className="me-xl-5" id="author"></p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-12">
+        <button className="btn btn-dark" onClick={getQuotes} id="new-quote">New Quote</button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/intent/tweet" className="me-2x"  id="tweet-quote"><i className="bi bi-twitter fa-2x"></i></a>
+        </div>
+        <div className="col-sm-4"></div>
+        <div className="col-sm-4"></div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12">
+        </div>
+      </div>
     </div>
+
   );
 }
+
 
 export default App;
